c82abbf3c9d840e5df1b0c9b44e25f76989a4957
Chris Baglieri <chrisbaglieri@chris-baglieris-macbook.local>
2011-01-29 17:29:43 -0500

    i give up on you hassle, for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,4 @@ Githacking::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  Sass::Plugin.options[:never_update] = true
 end
