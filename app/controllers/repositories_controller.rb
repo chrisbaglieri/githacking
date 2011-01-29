023c504ae031e656d1cf739729a1c09f0397ff3d
Josiah Kiehl <josiah@bluepojo.com>
2011-01-29 11:25:18 -0500

    UI infrastructure

---
+++ app/controllers/repositories_controller.rb
@@ -0,0 +1,23 @@
+class RepositoriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
