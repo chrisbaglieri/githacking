7c8b033ea8b3a2038f225fd3c4168abab3dc4f34
Josiah Kiehl <josiah@bluepojo.com>
2011-01-29 14:16:28 -0500

    Displaying issues on show

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,2 +1,13 @@
 module RepositoriesHelper
+  def github_user_url repo
+    "http://github.com/#{repo.user}"
+  end
+  
+  def github_repository_url repo
+    "#{github_user_url(repo)}/#{repo.name}"
+  end
+
+  def github_repository_issues_url repo, number
+    "#{github_repository_url(repo)}/issues#issue/#{number}"
+  end
 end
