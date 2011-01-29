a594831f4cd773be30a5e74f1f0ce2a37b880051
Josiah Kiehl <josiah@bluepojo.com>
2011-01-29 15:23:52 -0500

    Sort issues by gh tags

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -10,4 +10,11 @@ module RepositoriesHelper
   def github_repository_issues_url repo, number
     "#{github_repository_url(repo)}/issues#issue/#{number}"
   end
+
+  HUMAN_TAGS = {'gh-bitesize' => 'Bite Size', 'gh-easy' => 'Easy', 'gh-medium' => 'Medium', 'gh-hard' => 'Hard'}
+  GH_TAGS = ['gh-bitesize', 'gh-easy', 'gh-medium', 'gh-hard']
+
+  def human_tag tag
+    HUMAN_TAGS[tag]
+  end
 end
