614c821ee72e1403d662c1eb88bfc334fe41d9a4
Josiah Kiehl <josiah@bluepojo.com>
2011-01-29 14:05:25 -0500

    github interface for repositories

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -3,5 +3,8 @@ class Repository < ActiveRecord::Base
   validates_uniqueness_of :name, scope: :user
   
   acts_as_taggable
-  
+
+  def github
+    @github ||= Octopi::User.find(self.user).repository(self.name)
+  end
 end
