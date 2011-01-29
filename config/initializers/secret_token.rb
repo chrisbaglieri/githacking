d9a51585e67f373187d05bc54aa1c0557bd2d2ac
Chris Baglieri <chrisbaglieri@chris-baglieris-macbook.local>
2011-01-29 01:39:58 -0500

    go time!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githacking::Application.config.secret_token = '59f4ae3ff7f7305bda0dd7b5954ae6e3d2ee8cb64ade3c785a2daeb9a4252892d953ffc23b77b9c3d96b237547659162bd6ebe57adb0b159a9895ebece9c9ae7'
