fbf476d208d80952914361f8f1bdb8fe970feb33
Aaron Feng <aaron.feng@gmail.com>
2011-02-09 02:28:59 -0500

    removed comment and made title nullable

--- db/migrate/20110209061319_create_issues.rb
+++ db/migrate/20110209061319_create_issues.rb
@@ -1,14 +1,12 @@
 class CreateIssues < ActiveRecord::Migration
   def self.up
     create_table :issues do |t|
-      t.string    :position
-      t.integer   :number, :null => false, :default => 0
-      t.integer   :votes, :null => false, :default => 0
-      t.integer   :comment
-      t.text      :body,  :default => ""
-      t.string    :title, :null => false
-      t.string    :user, :null => false
-      t.string    :state, :null => false
+      t.integer   :number,            :null => false, :default => 0
+      t.integer   :votes,             :null => false, :default => 0
+      t.text      :body,                              :default => ""
+      t.string    :title,                             :default => ""
+      t.string    :user,              :null => false
+      t.string    :state,             :null => false
       t.timestamp :closed_at
       t.timestamp :created_at_github, :null => false
       t.timestamp :updated_at_github, :null => false
