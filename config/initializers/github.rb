2f07a2b6b96033ef9106fccf07aca89041bbaf33
Josiah Kiehl <josiah@bluepojo.com>
2011-02-12 16:56:22 -0500

    So we do need symbolize keys

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,8 +1,8 @@
 module Github
   def self.config
     github_config_file = File.join(Rails.root,'config','github.yml')
-    raise "#{github_config_file} is missing!" unless @@config or File.exists? github_config_file
+    raise "#{github_config_file} is missing!" unless defined?(@@config) or File.exists? github_config_file
 
-    @@config ||= YAML.load_file(github_config_file)[Rails.env]
+    @@config ||= YAML.load_file(github_config_file)[Rails.env].symbolize_keys
   end
 end
