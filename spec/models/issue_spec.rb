3967f142681e86b6c6eabcf20e58e50993d577f2
Aaron Feng <aaron.feng@gmail.com>
2011-02-11 22:08:49 -0500

    added issue model timestamps tests

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Issue do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @issue = Issue.new
+  end
+
+  describe "timestamps" do
+    before do
+      @now = Time.now
+    end
+
+    describe "when setting created_at" do
+      it "should set created_at_github" do
+        @issue.created_at = @now
+        @issue.created_at_github.should == @now
+      end
+
+      it "should set created_at_github" do
+        @issue.updated_at = @now
+        @issue.updated_at_github.should == @now
+      end
+    end
+  end
 end
