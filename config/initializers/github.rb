94026cc7f3c47ac8bac4858afc9924f988cf610f
Josiah Kiehl <josiah@bluepojo.com>
2011-02-12 16:29:33 -0500

    simplified github initializer

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,15 +1,8 @@
 module Github
   def self.config
-    unless defined?(@@config)
-      github_config_file = File.join(Rails.root,'config','github.yml')
-      raise "#{github_config_file} is missing!" unless File.exists? github_config_file
-      github_config = YAML.load_file(github_config_file)[Rails.env].symbolize_keys
-      @@config = {
-        client_id: github_config[:client_id],
-        secret: github_config[:secret],
-        redirect_uri: github_config[:redirect_uri]
-      }
-    end
-    @@config
+    github_config_file = File.join(Rails.root,'config','github.yml')
+    raise "#{github_config_file} is missing!" unless File.exists? github_config_file
+
+    @@config ||= YAML.load_file(github_config_file)[Rails.env].symbolize_keys
   end
 end
