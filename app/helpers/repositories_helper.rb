7e6b3c3f5ce95cc9bdf9a4f2f94a1593f367af69
Chris Baglieri <chris.baglieri@gmail.com>
2011-02-16 00:42:50 -0500

    added issues link on page title

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,5 +1,9 @@
-module RepositoriesHelper  
-  def github_repository_issues_url repo, number
-    "#{repo.url}/issues#issue/#{number}"
+module RepositoriesHelper
+  def github_repository_issues_url repo, number = 0
+    if number > 0
+      "#{repo.url}/issues#issue/#{number}"
+    else
+      "#{repo.url}/issues"
+    end
   end
 end
